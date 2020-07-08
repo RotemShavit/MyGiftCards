9a893ad17decf897d5fa7c02294ba260b937aba3
rotem-shavit <rotem.shavit@mail.huji.ac.il>
2020-07-08 22:07:34 +0300

    Created the activity's xml. Next time - adding logic

---
+++ app/src/main/java/com/postpc/mygiftcrads/AllGiftCardsActivity.kt
@@ -0,0 +1,12 @@
+package com.postpc.mygiftcrads
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllGiftCardsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_gift_cards)
+    }
+}
